2c91e10432b9b5b188d2604ea68b75e61e4c2862
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-03-03 10:02:40 -0500

    Initial commit on WebOMatic controlling class

---
+++ assignment_4/assignment_4/WebOMatic.java
@@ -0,0 +1,33 @@
+/**
+ * Controlling class, which is effectively a wrangler for costs and an
+ * invoice
+ *
+ * @author (Stewart Johnston)
+ * @version (2019-03-01.01)
+ */
+public class webOMatic
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class webOMatic
+     */
+    public webOMatic()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
