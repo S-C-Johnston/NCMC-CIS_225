b249cec07f53bbc1db5127d682772ad65db5a009
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-03-03 11:39:37 -0500

    Capitalize for style, strip BlueJ skeleton

--- assignment_4/assignment_4/WebOMatic.java
+++ assignment_4/assignment_4/WebOMatic.java
@@ -5,29 +5,16 @@
  * @author (Stewart Johnston)
  * @version (2019-03-01.01)
  */
-public class webOMatic
+public class WebOMatic
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Invoice invoice;
+    private DesignCosts designCosts;
 
     /**
-     * Constructor for objects of class webOMatic
+     * Constructor for objects of class WebOMatic
      */
-    public webOMatic()
+    public WebOMatic()
     {
-        // initialise instance variables
-        x = 0;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
